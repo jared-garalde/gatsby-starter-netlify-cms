cc264d8bb617e5006fefe8417a1794ae11de8396
Jared Garalde <jjgaralde@gmail.com>
2021-04-14 00:30:46 -0700

    add button styling to tags

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -33,7 +33,7 @@ export const BlogPostTemplate = ({
                 <ul className="taglist">
                   {tags.map((tag) => (
                     <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                      <Link className="button is-rounded" to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
                     </li>
                   ))}
                 </ul>
