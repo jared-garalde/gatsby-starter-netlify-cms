74202de0de242a99e34731fdc5c85b8d1bf5de4d
jared-garalde <jjgaralde@gmail.com>
2021-04-25 00:39:17 -0700

    Feat: Copy blog component to interviews
    Using the same format for interviews. Will need to add page associations
    so that interviews can be associated with episodes.

--- src/pages/interviews/index.js
+++ src/pages/interviews/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import BlogRoll from "../../components/BlogRoll";
 
 export default class InterviewsIndexPage extends React.Component {
   render() {
