5c2f0e5f9150b133ea334bedac0999aaf3c4ff93
jared-garalde <jjgaralde@gmail.com>
2021-04-25 00:26:09 -0700

    refactor: remove unused import statements interviews

--- src/pages/interviews/index.js
+++ src/pages/interviews/index.js
@@ -1,7 +1,5 @@
 import React from "react";
-
 import Layout from "../../components/Layout";
-import BlogRoll from "../../components/BlogRoll";
 
 export default class InterviewsIndexPage extends React.Component {
   render() {
