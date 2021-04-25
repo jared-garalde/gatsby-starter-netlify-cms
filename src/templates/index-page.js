a7be8d1dbab5b767033e2a36a73a99b953827d9f
jared-garalde <jjgaralde@gmail.com>
2021-04-25 11:35:32 -0700

    syntax: fix link error in listen now buttonw

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -48,7 +48,7 @@ export const IndexPageTemplate = ({ image, title, subheading, mainpitch }) => (
           {subheading}
         </p>
         <a
-          href="https://redcircle.com/shows/pinoys-vs-the-world/episodes/1b701b97-f85b-4c4c-9cde-66a04e91e114"
+          href="https://redcircle.com/shows/pinoys-vs-the-world"
           className="button is-primary"
         >
           Listen Now
