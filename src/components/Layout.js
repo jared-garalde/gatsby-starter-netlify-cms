fa2a0f4af49609fe9f5293b304ddae69be47e525
jared-garalde <jjgaralde@gmail.com>
2021-04-25 12:34:05 -0700

    Style: Change body font to @fontsource/inter
    From @fontsource/Monsterrat. Tested yarn and npm run build.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ import Navbar from "../components/Navbar";
 import "./all.sass";
 import useSiteMetadata from "./SiteMetadata";
 import { withPrefix } from "gatsby";
-import "@fontsource/montserrat";
+import "@fontsource/inter";
 
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata();
