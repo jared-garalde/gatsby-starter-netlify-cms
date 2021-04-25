b6cd0b88279ffda86e6ef9af3669496c6dab4bd1
jared-garalde <jjgaralde@gmail.com>
2021-04-25 11:01:27 -0700

    Refactor: change positioning of Navbar links to left

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,7 +63,7 @@ const Navbar = class extends React.Component {
             id="navMenu"
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
-            <div className="navbar-start has-text-centered">
+            <div className="navbar-end has-text-centered">
               <Link className="navbar-item" to="/about">
                 About
               </Link>
@@ -76,8 +76,6 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
               <div className="navbar-item">
                 <div className="grouped icon-text">
                   <a
